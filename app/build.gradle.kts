8267280bacf3a120de10c67ad0a65d1a9b660bc2
benayalaskar <benayalaskar2002@gmail.com>
2024-09-26 13:20:48 +0700

    Obfuscation config.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,14 +22,14 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
         debug {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
