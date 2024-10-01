a7b92221fb583aba38ab098071470ebb075dfcb3
benayalaskar <benayalaskar2002@gmail.com>
2024-10-01 09:53:13 +0700

    Add new FAB features.

--- app/src/main/java/com/benaya/movix/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/benaya/movix/ui/detail/DetailActivity.kt
@@ -47,6 +47,17 @@ class DetailActivity : AppCompatActivity() {
             detailViewModel.setFavoriteFilm(data, statusFavorite)
             setStatusFavorite(statusFavorite, true)
         }
+
+        // Add scroll listener to show/hide the favorite button when user scrolling the page
+        binding.scrollView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+            if (scrollY > oldScrollY) {
+                // User is scrolling down
+                binding.fabFav.hide()
+            } else if (scrollY < oldScrollY) {
+                // User is scrolling up
+                binding.fabFav.show()
+            }
+        }
     }
 
     private fun setStatusFavorite(statusFavorite: Boolean, onClick: Boolean) {
@@ -74,5 +85,4 @@ class DetailActivity : AppCompatActivity() {
     companion object {
         private const val DATA = "data_detail"
     }
-
 }
